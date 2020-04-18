e5b8a181fd93c47cc57170bd20c7d94ecbdc6d76
vv1ngs <litingfeng00@gmail.com>
2020-04-18 19:16:57 +0800

    some util

---
+++ src/main/java/org/hackDefender/util/HttpClientUtil.java
@@ -0,0 +1,41 @@
+package org.hackDefender.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+@Slf4j
+public class HttpClientUtil {
+    public static void getRequest(String url) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet get = new HttpGet(url);
+            HttpResponse httpResponse = httpClient.execute(get);
+            int code = httpResponse.getStatusLine().getStatusCode();
+            System.out.println(code);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void putRequest(String url, String str) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPut put = new HttpPut(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(100000).build();
+            put.setEntity(new StringEntity(str, ContentType.DEFAULT_TEXT));
+            put.setConfig(requestConfig);
+            HttpResponse httpResponse = httpClient.execute(put);
+            int code = httpResponse.getStatusLine().getStatusCode();
+            System.out.println(code);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/fprUtilTest/test.java
@@ -0,0 +1,9 @@
+package fprUtilTest;
+
+import org.hackDefender.util.FrpUtil;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(FrpUtil.rewriteFrp(123, 1234, 8080, 28000));
+    }
+}
